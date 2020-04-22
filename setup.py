736fdfe8b8e1b77344a02d3a843d705fad1bf8ad
Darius Aliulis <darius.aliulis@gmail.com>
2020-04-22 06:01:08 +0300

    chore: remove torch and unityagents requirements
    remove torch and unityagents setup.py to prevent
    auto setup from PyPi

--- setup.py
+++ setup.py
@@ -8,13 +8,16 @@ setup(
 
     install_requires=[
         "numpy",
-        "torch>=1.0, <2.0",
+        "pandas",
         "plotnine>=0.2.0",
         "click>=7.0.0",
-        "unityagents>=0.4.0", 'pandas'
     ],
     author="Darius Aliulis",
     author_email="darius.aliulis@gmail.com",
     description="Deep Q-Learning Agent for Unity Banana Navigation Environment",
     url="https://github.com/daraliu/drl-banana-navigation",
+    entry_points='''
+        [console_scripts]
+        banana-nav=banana_nav.cli:cli
+    '''
 )
